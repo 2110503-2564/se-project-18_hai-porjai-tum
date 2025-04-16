d3db22c155123064ed0ed8467211e3e255492ed6
Pann <pakkapon.pann@gmail.com>
2025-04-16 23:14:57 +0700

    lay out

---
+++ src/app/carinfo/car/manage/layout.tsx
@@ -0,0 +1,17 @@
+import RentalMenu from "@/components/RentalMenu";
+import getCars from "@/libs/getCars";
+
+export default function ManageBookingLayout(
+    { children}:
+        { children: React.ReactNode }
+) {
+    const carsPromise = getCars()
+    return (
+        <div className="flex flex-row pt-3">
+        <RentalMenu carsJson={carsPromise}/>
+        <div className="flex flex-col w-full">
+            {children}
+        </div>
+        </div>
+    )
+}
